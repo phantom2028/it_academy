b2ee9ebbdb1e3ac705a6a5f7acd736ca9e2acb3e
phantom2028 <dzonvik51@gmail.com>
2021-12-20 23:14:37 +0600

    Story benchmarks for speed

--- work2.py
+++ work2.py
@@ -1,4 +1,4 @@
-deposit = int(input('Введите сумму депозита :'))
+'''deposit = int(input('Введите сумму депозита :'))
 accumulated_deposit = int(input('Введите сумму накопления :'))
 interest_rate = int(input('Введите годовую ставку в процентах :'))
 
@@ -9,3 +9,8 @@ while deposit < accumulated_deposit:
     deposit = (deposit*(interest_rate/100)/12)+deposit
 
 print(f'Количество месяцев накопления нужной суммы :{month}')
+'''
+number = 10
+number_1 = 20
+result = (number - number_1)
+print(result)
